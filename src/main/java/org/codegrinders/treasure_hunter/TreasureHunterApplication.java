74a1be3eb7ba590d5fd96ecf24d4138395819b26
aneziou <ZXasqw1@>
2020-11-01 20:01:30 +0200

    initial commit

---
+++ src/main/java/org/codegrinders/treasure_hunter/TreasureHunterApplication.java
@@ -0,0 +1,13 @@
+package org.codegrinders.treasure_hunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureHunterApplication.class, args);
+    }
+
+}
