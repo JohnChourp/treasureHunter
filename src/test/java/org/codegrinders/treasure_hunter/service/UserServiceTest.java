95f70d833f699f86bf29c7aeca23ac0ac393a23f
athaderm <athanasia.dermanopoulou@outlook.com>
2020-12-07 16:10:34 +0200

    updated tests but they are still wrong..

--- src/test/java/org/codegrinders/treasure_hunter/service/UserServiceTest.java
+++ src/test/java/org/codegrinders/treasure_hunter/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package org.codegrinders.treasure_hunter.service;
 
+import org.codegrinders.treasure_hunter.exception.EmailIsAlreadyInUseException;
 import org.codegrinders.treasure_hunter.model.User;
 import org.codegrinders.treasure_hunter.repository.UserRepository;
 import org.junit.Assert;
@@ -16,7 +17,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
@@ -54,6 +55,27 @@ public class UserServiceTest {
         User user = new User("kotsos@kotsos.gr","kotsos","3333");
         userService.registerUser(user);
         Assert.assertNotNull(user.getDate());
+    }
+
+    @Test
+    public void when_a_user_with_similar_email_try_to_signUp_return_null() {
+        User user1 = new User("4", "pakis@pakis.gr", "joss", "111", 0,LocalDateTime.now());
+        userService.addUser(user1);
+        User user2 = new User("5", "pakis@pakispak.gr", "joss1", "111", 0,LocalDateTime.now());
+        when(userService.emailExists(user2.getEmail())).thenReturn(null);
+        assertNull(userService.addUser(user2));
+        //TODO error here!Test passes but it's wrong.
+    }
+
+    @Test(expected = EmailIsAlreadyInUseException.class)
+    public void when_a_user_exists_should_throw_exception(){
+
+        User user=new User("2","pakis@pakis.gr","sdf","sdf",0,LocalDateTime.now());
+        when(userService.addUser(user)).thenThrow(EmailIsAlreadyInUseException.class);
+        assertFalse(userService.emailExists(user.getEmail()));
+       // assertThrows(EmailIsAlreadyInUseException.class);
+        //TODO error here!
+
     }
 
 }
