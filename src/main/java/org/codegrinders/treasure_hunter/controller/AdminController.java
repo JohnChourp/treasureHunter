7eab368117f57c96483b3beb315596438033f17f
elenaserres <9BmDarE2>
2020-12-13 23:01:39 +0200

    Login page change.

--- src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
+++ src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
@@ -1,12 +1,17 @@
 package org.codegrinders.treasure_hunter.controller;
 
 import org.codegrinders.treasure_hunter.model.Puzzle;
+import org.codegrinders.treasure_hunter.service.PuzzleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+    @Autowired
+    PuzzleService puzzleService;
+
     @RequestMapping("/welcome")
     public void  sayHello(Model model)
     {
