962cf0f40ce7ea5d266cae5a1cef0cc1054003de
elenaserres <9BmDarE2>
2020-12-13 02:13:20 +0200

    change add puzzle form

--- src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
+++ src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
@@ -1,10 +1,17 @@
 package org.codegrinders.treasure_hunter.controller;
 
+import org.apache.catalina.LifecycleState;
 import org.codegrinders.treasure_hunter.model.Puzzle;
+import org.codegrinders.treasure_hunter.repository.PuzzleRepository;
 import org.codegrinders.treasure_hunter.service.PuzzleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
@@ -22,5 +29,4 @@ public class AdminController {
         }
         return "puzzle";
     }
-
 }
