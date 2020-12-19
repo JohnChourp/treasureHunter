7327eafd6ba0e60876dfe647a4ec1c7e34bd726a
elenaserres <9BmDarE2>
2020-12-19 18:15:51 +0200

    Issue completed.

--- src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
+++ src/main/java/org/codegrinders/treasure_hunter/controller/AdminController.java
@@ -1,23 +1,19 @@
 package org.codegrinders.treasure_hunter.controller;
 
-import org.apache.catalina.LifecycleState;
 import org.codegrinders.treasure_hunter.model.Puzzle;
-import org.codegrinders.treasure_hunter.repository.PuzzleRepository;
 import org.codegrinders.treasure_hunter.service.PuzzleService;
+import org.codegrinders.treasure_hunter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class AdminController {
 
     @Autowired
-    private PuzzleService puzzleService;
+    PuzzleService puzzleService;
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/login")
     public String login() {
@@ -35,8 +31,8 @@ public class AdminController {
             puzzleService.addPuzzle(puzzle);
         } catch (Exception e) {
             e.printStackTrace();
-            return "puzzle";
+            return "puzzles";
         }
-        return "puzzle";
+        return "puzzles";
     }
 }
