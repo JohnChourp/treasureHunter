e0600f5366c8eb66db1dbe747fe34a0d0d8b27a1
elenaserres <9BmDarE2>
2021-01-04 13:10:40 +0200

    setId in puzzle model added.

--- src/main/java/org/codegrinders/treasure_hunter/model/Puzzle.java
+++ src/main/java/org/codegrinders/treasure_hunter/model/Puzzle.java
@@ -65,6 +65,9 @@ public class Puzzle {
         this.points = points;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
     @Override
     public String toString() {
         return "Puzzle{" +
