a513f9b6c148d2bfd0c8e89b4fa19c42fd78c482
John <jonhiedarn@gmail.com>
2021-01-13 21:06:25 +0200

    Added commits from master

--- src/main/java/org/codegrinders/treasure_hunter/controller/PuzzleController.java
+++ src/main/java/org/codegrinders/treasure_hunter/controller/PuzzleController.java
@@ -38,7 +38,6 @@ public class PuzzleController {
     @GetMapping(value = "/answer")
     public Puzzle puzzleIsCorrect(@RequestParam("id") String puzzleId, @RequestParam("answer") String answer, @RequestParam("userId") String userId) {
         if (puzzleService.puzzleIsCorrect(puzzleId, answer)) {
-            markerService.updateVisibility(markerService.findMarkerByPuzzleId(puzzleId), false);
             userService.updatePoints(userId, puzzleService.findById(puzzleId).get().getPoints());
             userService.updateHasWon(userId);
             return new Puzzle(puzzleService.findById(puzzleId).get().getId(), puzzleService.findById(puzzleId).get().getAnswer());
